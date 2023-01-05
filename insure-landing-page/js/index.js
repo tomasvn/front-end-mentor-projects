0cdb311964996a1db8617d32254c00959a0ee0d0
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2023-01-05 01:32:09 +0100

    feat: insure landing page

--- insure-landing-page/js/index.js
+++ insure-landing-page/js/index.js
@@ -1,10 +1,14 @@
 const body = document.body;
 const menu = document.querySelector('.js-menu');
 const navbar = document.querySelector('.js-navbar');
+const icons = document.querySelectorAll(".icon");
+
 const handleClick = () => {
   body.classList.toggle('no-overflow');
   menu.classList.toggle('is-active');
   navbar.classList.toggle('is-active');
+
+  icons.forEach(ico => ico.classList.toggle("is-hidden"));
 }
 
-menu.addEventListener('click', handleClick)
+menu.addEventListener('click', handleClick);
